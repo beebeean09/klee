d1db2e42e7a2fd5557ab3b1a9ecf415222786d93
beebeean09 <beebeean09@gmail.com>
2017-07-24 20:58:13 -0700

    add images for splash page

--- krost/src/App.js
+++ krost/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="splash-img"></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
+// <img alt="splash" className="splash-img" src="../src/images/vlee1.jpg"/>
+// <img src={logo} className="App-logo" alt="logo" />
+// <h2>Welcome to React</h2>
+// <p className="App-intro">
+//   To get started, edit <code>src/App.js</code> and save to reload.
+// </p>
 export default App;
