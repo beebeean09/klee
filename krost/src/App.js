b61a53712aab71bacc6812b0f2a8813d2e661f05
beebeean09 <beebeean09@gmail.com>
2017-07-26 01:00:01 -0700

    change media queries to decrease font size and keep flex

--- krost/src/App.js
+++ krost/src/App.js
@@ -26,59 +26,62 @@ class App extends Component {
 // </div>
   render() {
     return (
+      <div className="main">
         <div id="bg">
           <div className="bg-img"></div>
-            <div className="sm-container">
-              <div className="navbar-right">
-                <a href="https://www.instagram.com/krostout/" target="_blank"
-                  rel="noopener noreferrer">
-                  <FontAwesome
-                    className='ig'
-                    name='instagram'
-                    size='3x'
-                    /></a>
-                <a href="https://www.facebook.com/krostout" target="_blank"
-                  rel="noopener noreferrer">
-                  <FontAwesome
-                    className='fb'
-                    name='facebook-square'
-                    size='3x'
+        </div>
+        <div className="sm-container">
+          <div className="navbar-right">
+            <a href="https://www.instagram.com/krostout/" target="_blank"
+              rel="noopener noreferrer">
+              <FontAwesome
+                className='ig'
+                name='instagram'
+                size='3x'
+                /></a>
+              <a href="https://www.facebook.com/krostout" target="_blank"
+                rel="noopener noreferrer">
+                <FontAwesome
+                  className='fb'
+                  name='facebook-square'
+                  size='3x'
                   /></a>
                 <a href="https://www.youtube.com/krostout" target="_blank"
                   rel="noopener noreferrer">
-                <FontAwesome
-                  className='yt'
-                  name='youtube-play'
-                  size='3x'
-                /></a>
-                <a href="https://www.soundcloud.com/krostout" target="_blank"
-                  rel="noopener noreferrer">
-                <FontAwesome
-                  className='sc'
-                  name='soundcloud'
-                  size='3x'
-                /></a>
-                <a href="http://www.krostout.bandcamp.com" target="_blank"
-                  rel="noopener noreferrer">
-                <FontAwesome
-                  className='bc'
-                  name='bandcamp'
-                  size='3x'
-                /></a>
-              </div>
-              <div className="name-header">
-                <p>K</p>
-                <p>R</p>
-                <p>O</p>
-                <p>S</p>
-                <p>T</p>
-              </div>
-              <div className="navbar">
-                <a className="sm-button" href="#">A b o u t</a>
-                <a className="sm-button" href="#">L i v e</a>
-              </div>
-            </div>
-        </div>
+                  <FontAwesome
+                    className='yt'
+                    name='youtube-play'
+                    size='3x'
+                    /></a>
+                  <a href="https://www.soundcloud.com/krostout" target="_blank"
+                    rel="noopener noreferrer">
+                    <FontAwesome
+                      className='sc'
+                      name='soundcloud'
+                      size='3x'
+                      /></a>
+                    <a href="http://www.krostout.bandcamp.com" target="_blank"
+                      rel="noopener noreferrer">
+                      <FontAwesome
+                        className='bc'
+                        name='bandcamp'
+                        size='3x'
+                        /></a>
+                    </div>
+                    <div className="name-header">
+                      <p>K</p>
+                      <p>R</p>
+                      <p>O</p>
+                      <p>S</p>
+                      <p>T</p>
+                    </div>
+                    <div className="navbar">
+                      <a className="sm-button" href="#">A b o u t</a>
+                      <a className="sm-button" href="#">L i v e</a>
+                    </div>
+                  </div>
+
+      </div>
     );
   }
 }
