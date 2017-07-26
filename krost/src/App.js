26abdd4747d85fc705a2643388177bf077485e83
beebeean09 <beebeean09@gmail.com>
2017-07-25 23:35:55 -0700

    change hover color and bio to about

--- krost/src/App.js
+++ krost/src/App.js
@@ -27,10 +27,6 @@ class App extends Component {
   render() {
     return (
         <div id="bg">
-          <div className="navbar">
-            <h1>Biography</h1>
-            <h1>Live</h1>
-          </div>
           <div className="bg-img"></div>
             <div className="sm-container">
               <div className="navbar-right">
@@ -39,43 +35,47 @@ class App extends Component {
                   <FontAwesome
                     className='ig'
                     name='instagram'
-                    size='2x'
+                    size='3x'
                     /></a>
-                  <a href="https://www.facebook.com/krostout" target="_blank"
-                    rel="noopener noreferrer">
-                    <FontAwesome
-                      className='fb'
-                      name='facebook-square'
-                      size='2x'
-                      /></a>
-                    <a href="https://www.youtube.com/krostout" target="_blank"
-                      rel="noopener noreferrer">
-                      <FontAwesome
-                        className='yt'
-                        name='youtube-play'
-                        size='2x'
-                        /></a>
-                      <a href="https://www.soundcloud.com/krostout" target="_blank"
-                        rel="noopener noreferrer">
-                        <FontAwesome
-                          className='sc'
-                          name='soundcloud'
-                          size='2x'
-                          /></a>
-                        <a href="http://www.krostout.bandcamp.com" target="_blank"
-                          rel="noopener noreferrer">
-                          <FontAwesome
-                            className='bc'
-                            name='bandcamp'
-                            size='2x'
-                            /></a>
-                        </div>
+                <a href="https://www.facebook.com/krostout" target="_blank"
+                  rel="noopener noreferrer">
+                  <FontAwesome
+                    className='fb'
+                    name='facebook-square'
+                    size='3x'
+                  /></a>
+                <a href="https://www.youtube.com/krostout" target="_blank"
+                  rel="noopener noreferrer">
+                <FontAwesome
+                  className='yt'
+                  name='youtube-play'
+                  size='3x'
+                /></a>
+                <a href="https://www.soundcloud.com/krostout" target="_blank"
+                  rel="noopener noreferrer">
+                <FontAwesome
+                  className='sc'
+                  name='soundcloud'
+                  size='3x'
+                /></a>
+                <a href="http://www.krostout.bandcamp.com" target="_blank"
+                  rel="noopener noreferrer">
+                <FontAwesome
+                  className='bc'
+                  name='bandcamp'
+                  size='3x'
+                /></a>
+              </div>
               <div className="name-header">
                 <p>K</p>
                 <p>R</p>
                 <p>O</p>
                 <p>S</p>
                 <p>T</p>
+              </div>
+              <div className="navbar">
+                <a className="sm-button" href="#">A b o u t</a>
+                <a className="sm-button" href="#">L i v e</a>
               </div>
             </div>
         </div>
