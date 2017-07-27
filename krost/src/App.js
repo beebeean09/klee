00f5ac574873a76fceaa847482975e8de43217fb
beebeean09 <beebeean09@gmail.com>
2017-07-27 14:30:47 -0700

    add about and live component with router;

--- krost/src/App.js
+++ krost/src/App.js
@@ -1,70 +1,21 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import FontAwesome from 'react-fontawesome';
+import { HashRouter, Route, Redirect } from 'react-router-dom';
+import Main from './components/Main.jsx';
+import About from './components/About.jsx';
+import Live from './components/Live.jsx';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-
   render() {
-    return (
-      <div className="main">
-        <div id="bg">
-          <div className="bg-img"></div>
+    return(
+      <HashRouter>
+        <div>
+          <Route exact path="/" component={ Main }/>
+          <Route path="/about" component={ About }/>
+          <Route path="/live" component={ Live }/>
         </div>
-        <div className="sm-container">
-          <div className="navbar-right">
-            <a href="https://www.instagram.com/krostout/" target="_blank"
-              rel="noopener noreferrer">
-              <FontAwesome
-                className='ig'
-                name='instagram'
-                size='3x'
-              /></a>
-            <a href="https://www.facebook.com/krostout" target="_blank"
-              rel="noopener noreferrer">
-              <FontAwesome
-                className='fb'
-                name='facebook-square'
-                size='3x'
-              /></a>
-            <a href="https://www.youtube.com/krostout" target="_blank"
-              rel="noopener noreferrer">
-              <FontAwesome
-                className='yt'
-                name='youtube-play'
-                size='3x'
-              /></a>
-            <a href="https://www.soundcloud.com/krostout" target="_blank"
-              rel="noopener noreferrer">
-              <FontAwesome
-                className='sc'
-                name='soundcloud'
-                size='3x'
-              /></a>
-            <a href="http://www.krostout.bandcamp.com" target="_blank"
-              rel="noopener noreferrer">
-              <FontAwesome
-                className='bc'
-                name='bandcamp'
-                size='3x'
-              /></a>
-          </div>
-          <div className="name-header">
-            <p>K</p>
-            <p>R</p>
-            <p>O</p>
-            <p>S</p>
-            <p>T</p>
-          </div>
-          <div className="navbar">
-            <a className="sm-button" href="#">A b o u t</a>
-            <a className="sm-button" href="#">L i v e</a>
-          </div>
-        </div>
-      </div>
+      </HashRouter>
     );
   }
 }
