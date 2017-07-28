afa995ac12220ec983e1d18dcb8a8868a4d63910
beebeean09 <beebeean09@gmail.com>
2017-07-27 21:34:30 -0700

    add in table for tour dates

--- krost/src/components/Live.jsx
+++ krost/src/components/Live.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import '../styles/live.css';
 
 class Live extends React.Component {
   render() {
     return(
-      <div>
-        
-
+      <div className="live-main">
+        <div className="live-banner"></div>
+        <div className="live-list">
+          <div className="live-list-items live-list-header">
+            <h1>Date</h1>
+            <h1>Location</h1>
+            <h1>Tickets</h1>
+          </div>
+          <div className="live-list-items">
+            <h1>July 30th</h1>
+            <h1>Orange County</h1>
+            <h1>FREE</h1>
+          </div>
+        </div>
       </div>
-
     );
   }
 }
