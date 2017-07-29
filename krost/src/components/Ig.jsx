60318639e77fe1489fde4bd96f7d7d3ef14c3a52
beebeean09 <beebeean09@gmail.com>
2017-07-28 17:49:46 -0700

    use instafeed to grab limit 10 imgs

--- krost/src/components/Ig.jsx
+++ krost/src/components/Ig.jsx
@@ -1,27 +1,45 @@
 import React from 'react';
 import * as KEY from './key.js';
+import Instafeed from 'instafeed.js';
+import '../styles/ig.css';
+
 
 class Ig extends React.Component {
   constructor(props) {
     super(props);
 
-
+    this.state = { info: ''};
   }
 
-  // fetchPhotos() {
-  //   fetch(
-  //     method: 'GET',
-  //     url: ''
-  //   ).then(res.)
-  // }
+  componentDidMount() {
+    this.fetchPhotos();
+  }
+
+  fetchPhotos() {
+    // fetch(`https://api.instagram.com/oauth/authorize/?client_id=${KEY.IG.client_id}&redirect_uri=${KEY.IG.redirect_uri}&response_type=token`)
+    //   .then(res => res.json())
+    //   .then(json => {
+    //     this.setState({info: json});
+    //   });
+    //   console.log(this.state.info);
+
+    let feed = new Instafeed({
+      get: 'user',
+      userId: `${KEY.IG.user_id}`,
+      accessToken: `${KEY.IG.token}`,
+      template: '<div id="ig-img"><a href="{{link}}"><img src="{{image}}" /></a>{{caption}}</div>',
+      limit: 10
+    });
+    feed.run();
+  }
 
 
   render() {
-    const key = KEY.IG;
-    console.log(key);
     return(
       <div>
         <h1>Instagram</h1>
+        <div id="instafeed">
+        </div>
       </div>
     );
   }
