7d59a6a75d7b00c38949096e97bc6e3f7647151d
beebeean09 <beebeean09@gmail.com>
2017-07-31 22:41:25 -0700

    change play button to fa-play

--- krost/src/components/Ig.jsx
+++ krost/src/components/Ig.jsx
@@ -33,13 +33,14 @@ class Ig extends React.Component {
                image.template = '<img src="' + image.images.standard_resolution.url + '" />';
            } else {
             //  fa-play, fa-play-circle, fa-play-circle-o
-               image.template = `<div style="position: relative; width: 293px; height: 293px; overflow: hidden;"><i class="fa fa-play-circle" aria-hidden="true"></i>
-             <video id=${image.id} controls><source src="` + image.videos.standard_resolution.url + '" type="video/mp4"/></video></div>';
+               image.template = `<div style="cursor: pointer; position: relative; width: 293px; height: 293px; overflow: hidden;">
+               <i class="fa fa-play" aria-hidden="true"></i><video id=${image.id} controls>
+                 <source src="` + image.videos.standard_resolution.url + '" type="video/mp4"/></video></div>';
            }
            return true;
        },
       template: '<div id="ig-img">{{model.template}}</div>',
-      limit: 20
+      limit: 30
     });
     feed.run();
   }
