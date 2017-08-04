11ce45d415d674618a0dbed6b73dce11a477926e
beebeean09 <beebeean09@gmail.com>
2017-08-03 21:42:05 -0700

    remove router and place all comps into App

--- krost/src/App.js
+++ krost/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import {Route, Switch, BrowserRouter } from 'react-router-dom';
 import Main from './components/Main.jsx';
 import About from './components/About.jsx';
 import Live from './components/Live.jsx';
@@ -9,14 +8,12 @@ import Ig from './components/Ig.jsx';
 class App extends Component {
   render() {
     return(
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Main }/>
-          <Route path="/about" component={ About }/>
-          <Route path="/live" component={ Live }/>
-          <Route path="/ig" component={ Ig }/>
-        </Switch>
-      </BrowserRouter>
+      <div className="main">
+        <Main />
+        <About />
+        <Live />
+        <Ig />
+      </div>
     );
   }
 }
--- krost/src/components/Main.jsx
+++ krost/src/components/Main.jsx
@@ -25,66 +25,61 @@ class Main extends Component {
 
   render() {
     return (
-      <div className="main">
-        <div className="main-wrapper">
-          <div className="bg">
-            <div className="bg-img bg-img-main"></div>
-            <div className="sm-container bg-img">
-              <div className="navbar-right">
-                <a href="https://www.instagram.com/krostout/" target="_blank"
-                  rel="noopener noreferrer">
+      <div className="main-wrapper">
+        <div className="bg">
+          <div className="bg-img bg-img-main"></div>
+          <div className="sm-container bg-img">
+            <div className="navbar-right">
+              <a href="https://www.instagram.com/krostout/" target="_blank"
+                rel="noopener noreferrer">
+              <FontAwesome
+                className='ig'
+                name='instagram'
+                size='3x'
+              /></a>
+              <a href="https://www.facebook.com/krostout" target="_blank"
+                rel="noopener noreferrer">
                 <FontAwesome
-                  className='ig'
-                  name='instagram'
+                  className='fb'
+                  name='facebook-square'
                   size='3x'
-                /></a>
-                <a href="https://www.facebook.com/krostout" target="_blank"
-                  rel="noopener noreferrer">
-                  <FontAwesome
-                    className='fb'
-                    name='facebook-square'
-                    size='3x'
-                /></a>
-                <a href="https://www.youtube.com/krostout" target="_blank"
-                  rel="noopener noreferrer">
-                  <FontAwesome
-                    className='yt'
-                    name='youtube-play'
-                    size='3x'
-                /></a>
-                <a href="https://www.soundcloud.com/krostout" target="_blank"
-                  rel="noopener noreferrer">
-                  <FontAwesome
-                    className='sc'
-                    name='soundcloud'
-                    size='3x'
-                /></a>
-                <a href="http://www.krostout.bandcamp.com" target="_blank"
-                  rel="noopener noreferrer">
-                  <FontAwesome
-                    className='bc'
-                    name='bandcamp'
-                    size='3x'
-                /></a>
-              </div>
-              <div className="name-header">
-                <p>K</p>
-                <p>R</p>
-                <p>O</p>
-                <p>S</p>
-                <p>T</p>
-              </div>
-              <div className="navbar">
-                <a className="sm-button" href="#about-link">A b o u t</a>
-                <a className="sm-button" href="#live-link">L i v e</a>
-              </div>
+              /></a>
+              <a href="https://www.youtube.com/krostout" target="_blank"
+                rel="noopener noreferrer">
+                <FontAwesome
+                  className='yt'
+                  name='youtube-play'
+                  size='3x'
+              /></a>
+              <a href="https://www.soundcloud.com/krostout" target="_blank"
+                rel="noopener noreferrer">
+                <FontAwesome
+                  className='sc'
+                  name='soundcloud'
+                  size='3x'
+              /></a>
+              <a href="http://www.krostout.bandcamp.com" target="_blank"
+                rel="noopener noreferrer">
+                <FontAwesome
+                  className='bc'
+                  name='bandcamp'
+                  size='3x'
+              /></a>
+            </div>
+            <div className="name-header">
+              <p>K</p>
+              <p>R</p>
+              <p>O</p>
+              <p>S</p>
+              <p>T</p>
+            </div>
+            <div className="navbar">
+              <a className="sm-button" href="#about-link">A b o u t</a>
+              <a className="sm-button" href="#live-link">L i v e</a>
             </div>
           </div>
         </div>
-      <About />
-      <Live />
-      <Ig />
-    </div>
+      </div>
     );
   }
 }
